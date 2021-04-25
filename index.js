bc7ae05bf8376e097167b1012d1c6afab2c5cedb
JaSON <masteraccounthub@gmail.com>
2021-04-25 18:27:05 -0400

    commit tree be0f245fa9ed6bc625df8ee94e7e3b15956654aa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(fun="roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+
+function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
